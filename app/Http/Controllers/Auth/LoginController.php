d813ac7f705d483d725c9f21aec9658308325c10
bagoeswn <bagoeswn@gmail.com>
2022-10-04 13:13:24 +0700

    fix login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,29 +41,29 @@ class LoginController extends Controller
         }
     }
 
-    public function login(Request $request) {
-        $input = $request->all();
-        $this->validate($request,[
-            // 'email' => 'required|email|exists:users,email',
-            'password'=>'required'
-        ],[
-            'name.exists' => 'Username tidak terdaftar',
+    // public function login(Request $request) {
+    //     $input = $request->all();
+    //     $this->validate($request,[
+    //         // 'email' => 'required|email|exists:users,email',
+    //         'password'=>'required'
+    //     ],[
+    //         'name.exists' => 'Username tidak terdaftar',
 
-        ]);
-        if(auth()->attempt(array('name'=>$input['name'], 'password'=> $input['password'])) ) {
-            if(Auth()->user()->level == 1){
-                return route('home1');
-            }
-            elseif(Auth()->user()->role == 2){
-                return route('home2');
-            }
-            elseif (Auth()->user()->role == 3) {
-                return route('home3');
-            }
-        }else {
-            return redirect()->back()->with('error','Gagal login');
-        }
-    }
+    //     ]);
+    //     if(auth()->attempt(array('name'=>$input['name'], 'password'=> $input['password'])) ) {
+    //         if(Auth()->user()->level == 1){
+    //             return route('home1');
+    //         }
+    //         elseif(Auth()->user()->role == 2){
+    //             return route('home2');
+    //         }
+    //         elseif (Auth()->user()->role == 3) {
+    //             return route('home3');
+    //         }
+    //     }else {
+    //         return redirect()->back()->with('error','Gagal login');
+    //     }
+    // }
 
     /**
      * Create a new controller instance.
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/level1';
 
     /**
      * Define your route model bindings, pattern filters, etc.
